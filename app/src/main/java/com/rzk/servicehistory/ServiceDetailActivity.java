67507bedb79ee8a194609b0c5cd14b533b984548
rkalgasi <rkalgasi@gmail.com>
2015-04-30 17:23:02 +0700

    edit rootmenu and service detail

--- app/src/main/java/com/rzk/servicehistory/ServiceDetailActivity.java
+++ app/src/main/java/com/rzk/servicehistory/ServiceDetailActivity.java
@@ -16,24 +16,24 @@ public class ServiceDetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_service);
-        Intent intent=getIntent();
+        Intent intent = getIntent();
 
-        if(intent!=null){
-            Bundle serviceInfo=intent.getExtras();
-            TextView textView=(TextView)findViewById(R.id.tv_service_name);
+        if (intent != null) {
+            Bundle serviceInfo = intent.getExtras();
+            TextView textView = (TextView) findViewById(R.id.tv_service_name);
             textView.setText(serviceInfo.getString("serviceName"));
-            textView=(TextView)findViewById(R.id.tv_date);
+            textView = (TextView) findViewById(R.id.tv_date);
             textView.setText(serviceInfo.getString("serviceDate"));
-            textView=(TextView) findViewById(R.id.tv_spare);
+            textView = (TextView) findViewById(R.id.tv_spare);
             textView.setText(serviceInfo.getString("serviceSparePart"));
-            textView=(TextView) findViewById(R.id.tv_info);
+            textView = (TextView) findViewById(R.id.tv_info);
             textView.setText(serviceInfo.getString("serviceInfo"));
-            Toast.makeText(this,"Data Received",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Data Received", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void backToMenu(View view){
+    public void backToMenu(View view) {
         finish();
     }
-  
+
 }
